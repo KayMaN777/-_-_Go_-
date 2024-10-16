bd084795aa7cfda5f5e319a055751ae5e504ae73
Кайгородов Максим Николаевич <mnkaygorodov_1@edu.hse.ru>
2024-10-16 12:19:54 +0300

    Fix requests, answering only correct method

--- task2/internal/server/handler.go
+++ task2/internal/server/handler.go
@@ -13,37 +13,49 @@ import (
 const version = "v1.0.0"
 
 func VersionHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, version)
+	if r.Method == "GET" {
+		fmt.Fprintf(w, version)
+	} else {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
 }
 
 func DecodeHandler(w http.ResponseWriter, r *http.Request) {
-	var req model.DecodeRequest
-	err := json.NewDecoder(r.Body).Decode(&req)
-	if err != nil {
-		http.Error(w, "invalid request", http.StatusBadRequest)
-		return
-	}
+	if r.Method == "POST" {
+		var req model.DecodeRequest
+		err := json.NewDecoder(r.Body).Decode(&req)
+		if err != nil {
+			http.Error(w, "invalid request", http.StatusBadRequest)
+			return
+		}
 
-	decoded, err := base64.StdEncoding.DecodeString(req.InputString)
-	if err != nil {
-		http.Error(w, "invalid base64 string", http.StatusBadRequest)
-		return
-	}
+		decoded, err := base64.StdEncoding.DecodeString(req.InputString)
+		if err != nil {
+			http.Error(w, "invalid base64 string", http.StatusBadRequest)
+			return
+		}
 
-	res := model.DecodeResponse{
-		OutputString: string(decoded),
+		res := model.DecodeResponse{
+			OutputString: string(decoded),
+		}
+		json.NewEncoder(w).Encode(res)
+	} else {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
-	json.NewEncoder(w).Encode(res)
 }
 
 func HardOpHandler(w http.ResponseWriter, r *http.Request) {
-	sleepTime := rand.Intn(11) + 10 // from 10 to 20 seconds
-	time.Sleep(time.Duration(sleepTime) * time.Second)
+	if r.Method == "GET" {
+		sleepTime := rand.Intn(11) + 10 // from 10 to 20 seconds
+		time.Sleep(time.Duration(sleepTime) * time.Second)
 
-	if rand.Intn(2) == 0 {
-		http.Error(w, "internal server error", http.StatusInternalServerError)
+		if rand.Intn(2) == 0 {
+			http.Error(w, "internal server error", http.StatusInternalServerError)
+		} else {
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprint(w, "Operation completed successfully")
+		}
 	} else {
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprint(w, "Operation completed successfully")
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
 }
